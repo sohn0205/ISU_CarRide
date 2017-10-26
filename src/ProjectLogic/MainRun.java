bcbff01ccfe6d7be7d2ef7d9680f6305e84395dd
Daniel Tidyman <fendersfolly@icloud.com>
2017-10-25 21:16:31 -0500

    Added a new Main Method to test Derby implementation
    Added a new Main Method to test out Derby

---
+++ src/ProjectLogic/MainRun.java
@@ -0,0 +1,20 @@
+package ProjectLogic;
+
+import javax.xml.crypto.Data;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MainRun {
+
+    public static void main(String args[]) throws
+            SQLException, IOException, ClassNotFoundException {
+
+        Database.init();
+        
+
+        }
+
+    }
+}
